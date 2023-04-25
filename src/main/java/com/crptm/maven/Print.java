c2b52abca0b2d411c1e27b353f20511c48a1f7ac
Sandeep Saini <sandeep.saini@crptm.com>
2023-04-25 15:08:15 +0530

    new

--- src/main/java/com/crptm/maven/Print.java
+++ src/main/java/com/crptm/maven/Print.java
@@ -5,4 +5,8 @@ public final class Print {
     public static void ln(final String msg) {
         System.out.println(msg);
     }
+
+    public static void nln(final String msg) {
+        System.out.print(msg);
+    }
 }
